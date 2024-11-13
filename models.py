fd18024a7d3239d0873c1327d3e70cee2813a7ce
ModSecurity UI <modsec-ui@bravo.com>
2024-11-13 17:04:41 +0700

    Modified Rules: - REQUEST-901-INITIALIZATION.conf

--- models.py
+++ models.py
@@ -14,7 +14,7 @@ class ModsecRule(db.Model):
     __tablename__ = 'modsec_rules'
 
     id = db.Column(db.Integer, primary_key=True)
-    rule_code = db.Column(db.String(10), nullable=False)
+    rule_code = db.Column(db.String(20), nullable=False)
     rule_name = db.Column(db.String(100), nullable=False)
     rule_path = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
