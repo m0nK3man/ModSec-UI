3fe82de53bffbb2c00fff5dae10c6134f775b3ce
Your Name <your@example.com>
2024-11-14 11:30:42 +0700

    update script bk

--- scripts/backup_users.sh
+++ scripts/backup_users.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-if [ -e /tmp/modsec_users.sql ]; then
-    echo "File /tmp/modsec_users.sql exists"
+DB=modsec_ui
+TB=modsec_users
+if [ -e /tmp/$TB.sql ]; then
+    echo "File /tmp/$TB.sql exists"
 else
-    sudo -u postgres psql -d modsec_ui -c "\! pg_dump -d modsec_ui -t modsec_users -F p -f /tmp/modsec_users.sql"
-    echo "File backup successfully to /tmp/modsec_users.sql"
+    sudo -u postgres psql -d $DB -c "\! pg_dump -d $DB -t $TB -F p -f /tmp/$TB.sql"
+    echo "File backup successfully to /tmp/$TB.sql"
+    sudo mv /tmp/$TB.sql $(pwd)/database/$TB.sql
+    echo "File moved to $(pwd)/database/$TB.sql"
 fi
 
